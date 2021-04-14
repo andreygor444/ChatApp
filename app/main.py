e1581f7b0c142010fe01c00cb4a3c3c189d5806d
Andrey <andreygor444@gmail.com>
2021-04-14 20:26:34 +0300

    Выделил отдельные директории под аватары пользователей и чатов; Загрузил дефолтные аватары чатов; Изменил хост чтобы было проще работать на Windows

--- app/main.py
+++ app/main.py
@@ -25,57 +25,57 @@ unique_codes_manager.generate_unique_codes(10)
 
 @login_manager.user_loader
 def load_user(user_id):
-	return create_session().query(User).get(user_id)
+    return create_session().query(User).get(user_id)
 
 
 @login_required
 @app.route('/')
 def home_page():
-	return render_template("index.html")
+    return render_template("index.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def authorization():
-	form = LoginForm()
-	if form.validate_on_submit():
-		user = find_user_by_email(create_session(), form.email.data)
-		if user and user.check_password(form.password.data):
-			login_user(user, remember=form.remember_me.data)
-			return redirect('/')
-		return render_template("login.html", message="Неправильный логин или пароль", form=form)
-	return render_template("login.html", form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = find_user_by_email(create_session(), form.email.data)
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect('/')
+        return render_template("login.html", message="Неправильный логин или пароль", form=form)
+    return render_template("login.html", form=form)
 
 
 @app.route("/register/<unique_code>", methods=["GET", "POST"])
 def registration(unique_code):
-	if not unique_codes_manager.check_code(unique_code):
-		abort(403)
-	#unique_codes_manager.update_code(unique_code)
-	form = RegisterForm()
-	if form.validate_on_submit():
-		db_sess = create_session()
-		if find_user_by_email(db_sess, form.email.data):
-			return render_template("register.html", form=form, message="Пользователь с таким адресом электронной почты уже зарегистрирован")
-		add_user(db_sess, form.name.data, form.surname.data, form.email.data, form.password.data)
-		return render_template("registered_successfully.html")
-	return render_template("register.html", form=form)
+    if not unique_codes_manager.check_code(unique_code):
+        abort(403)
+    #unique_codes_manager.update_code(unique_code)
+    form = RegisterForm()
+    if form.validate_on_submit():
+        db_sess = create_session()
+        if find_user_by_email(db_sess, form.email.data):
+            return render_template("register.html", form=form, message="Пользователь с таким адресом электронной почты уже зарегистрирован")
+        add_user(db_sess, form.name.data, form.surname.data, form.email.data, form.password.data)
+        return render_template("registered_successfully.html")
+    return render_template("register.html", form=form)
 
 
 @login_required
 @app.route("/chats")
 def chat_list():
-	db_sess = create_session()
-	user_chats = get_user_chats(db_sess, current_user)
-	user_chats.sort(key=lambda chat: chat.last_message.dispatch_date)
-	notifications = current_user.get_notifications_dict()
-	print(notifications)
-	user_chats.sort(key=lambda chat: notifications[chat.id], reverse=True)
-	return render_template("chats.html", user_chats=user_chats)
+    db_sess = create_session()
+    user_chats = get_user_chats(db_sess, current_user)
+    user_chats.sort(key=lambda chat: chat.last_message.dispatch_date)
+    notifications = current_user.get_notifications_dict()
+    print(notifications)
+    user_chats.sort(key=lambda chat: notifications[chat.id], reverse=True)
+    return render_template("chats.html", user_chats=user_chats)
 
 
 def main():
-	app.run(host="0.0.0.0", port=3838, debug=True)
+    app.run(host="127.0.0.1", port=3838, debug=True)
 
 
 if __name__ == "__main__":
-	main()
+    main()
